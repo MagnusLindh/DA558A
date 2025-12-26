d2318493be1a2861885045402566400cd4b4e414
MagnusLindh <gnumas@hotmail.com>
2025-12-26 17:54:10 +0100

    Add validation and clear functions

--- contact.js
+++ contact.js
@@ -3,7 +3,8 @@ console.log(hopp);
 
 let fname,lname,email,mobile,subject,message;
 
-function storeFormElements() {
+function startUp() {
+    // store form element
     fname=document.getElementById("myform").elements["fname"];
     lname=document.getElementById("myform").elements["lname"];
     email=document.getElementById("myform").elements["email"];
@@ -16,6 +17,91 @@ function storeFormElements() {
     console.log(mobile);
     console.log(subject);
     console.log(message);
+    // start event listeners
+    fname.oninput = function() {
+        let input = fname.value;
+        if (!validateName(input)){
+            showError("fnameErrorMessage","Use only letters.");
+        } else {
+            clearError("fnameErrorMessage");
+        }
+    };
+    lname.oninput = function() {
+        let input = lname.value;
+        if (!validateName(input)){
+            showError("lnameErrorMessage","Use only letters.");
+        } else {
+            clearError("lnameErrorMessage");
+        }
+    };
+    email.oninput = function() {
+        let input = email.value;
+        if (!validateEmail(input)){
+            showError("emailErrorMessage","Use @ and a valid domain name.");
+        } else {
+            clearError("emailErrorMessage");
+        }
+    };
+    message.oninput = function() {
+        let input = message.value;
+        document.getElementById("counter").innerHTML=input.length+"/20";
+        if (!validateMessage(input)){
+            showError("messageErrorMessage","Use at least 20 letters.");
+            document.getElementById("counter").style.color="red";
+        } else {
+            clearError("messageErrorMessage");
+            document.getElementById("counter").style.color="green";
+        }
+    }
 }
 
-window.onload = storeFormElements;
+
+
+window.onload = startUp;
+
+function validateField(input, condition, errorMsg) {
+    if (!condition) {
+        // show error
+        return false;
+    } else {
+        // clear error
+        return true;
+    }
+}
+
+function validateName(input) {
+    const nameRegex  = /^[A-Za-z]+$/;
+    return nameRegex.test(input);
+}
+
+function validateEmail(input) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(input);
+}
+
+function validateMessage(input){
+    return input.length>=20;
+}
+
+function showError(element,error){
+    document.getElementById(element).innerHTML = error;
+}
+
+function clearError(element){
+    document.getElementById(element).innerHTML = "";
+}
+
+function clearForm(){
+    document.getElementById("fname").innerHTML="";
+    document.getElementById("lname").innerHTML="";
+    document.getElementById("email").innerHTML="";
+    document.getElementById("mobile").innerHTML="";
+    document.getElementById("message").innerHTML="";
+}
+
+function checkArray(input,condition){
+    for (let i = 0; i < input.length; i++){
+    // do something with array[i]
+    }
+}
+    
