023d7793ad239411aaa9d2bbbd7c95ea579a8877
MagnusLindh <gnumas@hotmail.com>
2026-01-01 10:59:13 +0100

    Add phone regex

--- contact.js
+++ contact.js
@@ -16,24 +16,40 @@ function startUp() {
         let input = fname.value;
         if (!validateName(input)){
             showError("fnameErrorMessage","Use only letters.");
+            fname.style.border = "solid red"; 
         } else {
             clearError("fnameErrorMessage");
+            fname.style.border = "solid green"; 
         }
     };
     lname.oninput = function() {
         let input = lname.value;
         if (!validateName(input)){
             showError("lnameErrorMessage","Use only letters.");
+            lname.style.border = "solid red"; 
         } else {
             clearError("lnameErrorMessage");
+            lname.style.border = "solid green";
         }
     };
     email.oninput = function() {
         let input = email.value;
         if (!validateEmail(input)){
             showError("emailErrorMessage","Use @ and a valid domain name.");
+            email.style.border = "solid red"; 
         } else {
             clearError("emailErrorMessage");
+            email.style.border = "solid green";
+        }
+    };
+    mobile.oninput = function() {
+        let input = mobile.value;
+        if (!validateMobile(input)){
+            showError("mobileErrorMessage","Use a valid phone number.");
+            email.style.border = "solid red"; 
+        } else {
+            clearError("mobileErrorMessage");
+            email.style.border = "solid green";
         }
     };
     message.oninput = function() {
@@ -42,9 +58,11 @@ function startUp() {
         if (!validateMessage(input)){
             showError("messageErrorMessage","Use at least 20 characters.");
             document.getElementById("counter").style.color="red";
+            message.style.border = "solid red"; 
         } else {
             clearError("messageErrorMessage");
             document.getElementById("counter").style.color="green";
+            message.style.border = "solid green";
         }
     }
 }
@@ -52,16 +70,6 @@ function startUp() {
 
 
 window.onload = startUp;
-
-function validateField(input, condition, errorMsg) {
-    if (!condition) {
-        // show error
-        return false;
-    } else {
-        // clear error
-        return true;
-    }
-}
 
 function validateName(input) {
     const nameRegex  = /^[A-Za-z]+$/;
@@ -73,16 +81,22 @@ function validateEmail(input) {
     return emailRegex.test(input);
 }
 
+function validateMobile(input) {
+    const mobileRegex =  /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+    return mobileRegex.test(input);
+}
+
 function validateMessage(input){
     return input.length>=20;
 }
 
 function showError(element,error){
+    document.getElementById(element).style.opacity="100%";
     document.getElementById(element).innerHTML = error;
 }
 
 function clearError(element){
-    document.getElementById(element).innerHTML = "";
+    document.getElementById(element).style.opacity="0%";
 }
 
 function clearForm(){
@@ -109,21 +123,24 @@ function validateSubmission(event){
         output=output + "Use @ and a valid domain name in email adress.";
         errors++;
     }
+    if (!validateMobile(mobile.value)){
+        output=output + "Use a valid phone number.";
+        errors++;
+    }
     if (!validateMessage(message.value)){
         output=output + "Use at least 20 characters in message text.";
         errors++;
     }
 
-    showError("submitErrorMessage",output);
+    showError("submitMessage",output);
 
     if (errors==0){
+        showError("submitMessage","Thank you " + fname.value + ", we will contact you as soon as possible.");
         clearForm();
+        setTimeout(() => {
+            showError("submitMessage","");
+          }, "3000");
     }
 }
 
-function checkArray(input,condition){
-    for (let i = 0; i < input.length; i++){
-    // do something with array[i]
-    }
-}
     
